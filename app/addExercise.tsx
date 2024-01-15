698708112733a351808fa3c4d3beb20df1ea79af
Nikhil Nagar <nikhilnagarcps@gmail.com>
2024-01-15 13:11:54 +0530

    multiselect functionality added

--- app/addExercise.tsx
+++ app/addExercise.tsx
@@ -12,88 +12,59 @@ import exerciseData from "../constants/exercise_data";
 
 const data = exerciseData;
 
-const ListItem = ({ item, selected, onPress, onLongPress }: any) => (
-  <>
-    <TouchableOpacity
-      onPress={onPress}
-      onLongPress={onLongPress}
-      style={styles.listItem}>
-      <View className="flex flex-row items-center gap-2">
-        <View className="rounded-full  w-12 h-12 border border-violet-300">
-          <Image className="rounded-full w-full h-full " source={item.icon} />
-        </View>
-        <View>
-          <Text>
-            {item.name} ({item.equipment})
-          </Text>
-          <Text className=" text-gray-400">{item.bodypart}</Text>
-        </View>
-      </View>
-      {selected && <View style={styles.overlay} />}
-    </TouchableOpacity>
-  </>
-);
-
-const App = () => {
+const AddExercisePage = () => {
   const [selectedItems, setSelectedItems] = useState<string[]>([]);
-  const handleOnPress = (contact: any) => {
-    if (selectedItems.length) {
-      return selectItems(contact);
+  const handleOnPress = (id: any) => {
+    if (selectedItems.includes(id)) {
+      setSelectedItems(selectedItems.filter((itemid) => itemid !== id));
+    } else {
+      setSelectedItems([...selectedItems, id]);
     }
-
-    // here you can add you code what do you want if user just do single tap
-    console.log("pressed");
-  };
-
-  const getSelected = (contact: any) => selectedItems.includes(contact.id);
-
-  const deSelectItems = () => setSelectedItems([]);
-
-  const selectItems = (item: any) => {
-    if (selectedItems.includes(item.id)) {
-      const newListItems = selectedItems.filter(
-        (listItem) => listItem !== item.id
-      );
-      return setSelectedItems([...newListItems]);
-    }
-    setSelectedItems([...selectedItems, item.id]);
   };
 
   return (
-    <Pressable onPress={deSelectItems}>
+    <View className="">
       <FlatList
         data={data}
         renderItem={({ item }) => (
-          <ListItem
-            onPress={() => handleOnPress(item)}
-            onLongPress={() => selectItems(item)}
-            selected={getSelected(item)}
+          <ExerciseCard
             item={item}
-          />
+            handleOnPress={handleOnPress}
+            selectedItems={selectedItems}></ExerciseCard>
         )}
         keyExtractor={(item) => item.id}
-        ItemSeparatorComponent={() => <View className="p-1"></View>}
+        ItemSeparatorComponent={() => <View className="p-2"></View>}
+        ListHeaderComponent={() => <View className="py-2"></View>}
       />
-    </Pressable>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {},
-  listItem: {
-    // backgroundColor: "#252628",
-    // marginBottom: 10,
-    // borderRadius: 5,
-    // overflow: "hidden",
-  },
-  overlay: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    backgroundColor: "rgba(160,20,130,0.2)",
-  },
-});
+const ExerciseCard = ({ item, handleOnPress, selectedItems }: any) => (
+  <TouchableOpacity
+    onPress={() => {
+      handleOnPress(item.id);
+    }}>
+    <View
+      className="flex flex-row items-center gap-3 pl-2"
+      style={selectedItems.includes(item.id) ? styles.background : styles.none}>
+      <View className="rounded-full  w-12 h-12 border border-violet-300">
+        <Image className="rounded-full w-full h-full" source={item.icon} />
+      </View>
+      <View>
+        <Text>
+          {item.name} ({item.equipment})
+        </Text>
+        <Text className=" text-gray-400">{item.bodypart}</Text>
+      </View>
+    </View>
+  </TouchableOpacity>
+);
 
-export default App;
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "red",
+  },
+  none: {},
+});
+export default AddExercisePage;
